ecf5dd672dcd0dd38251024171d5caee223af7da
Anyelo1813 <anyeldelarosa821@gmail.com>
2025-02-09 20:54:12 -0400

    Cambios Realizados

---
+++ BehiculosBlazor/Domain/Dto/VehiculoDto.cs
@@ -0,0 +1,20 @@
+﻿namespace VehiculosBlazor.Domain.BehiculoDto
+{
+    public class VehiculoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public string Tipo { get; set; }
+        public string Placa { get; set; }
+        public string Chasis { get; set; }
+        public string Motor { get; set; }
+        public string Año { get; set; }
+       
+        public string Estado { get; set; }
+        
+
+    }
+}
